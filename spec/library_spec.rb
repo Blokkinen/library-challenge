7f320b69e32981e3658706cb9e31e10cb310a1be
blokkinen <viktoria_blok@hotmail.com>
2016-08-14 23:19:56 +0200

    added a working test to see the library

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,7 +1,9 @@
+require './lib/library.rb'
+
 describe Library do
 
   it 'should be able to see a collection of books in the library' do
-    expect(subject.@items)
+    expect(subject.items).to be_truthy
 
   end
 end
